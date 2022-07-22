b3ed010473b916347ebe723adb8d82491440adc1
fredkk22 <freddykwak123@gmail.com>
2022-07-21 20:04:10 -0400

    Edited README.md and script.js files
    Added comments to specify sections of code for easier perusal.
    Added all the sections to the README.md file, included a video link instead of screenshots of the application.

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Password-Generator
+
+## Table of Contents
+- [Description](#description)
+- [Video Recording of Password Generator](#video-recording)
+- [Credits](#credits)
+- [Link](#link-to-deployed-application)
+
+## Description
+
+The Module 3 challenge was to create a random password generator using a script.js file.
+
+The reason for creating a random password generator was to learn more about Javascript methods, for loops, functions, if statements, confirms, alerts, prompts, and events.
+
+I was able to learn more about and become more comfortable with media queries, flexbox, CSS grids, and defining/applying CSS variables throughout this challenge.
+
+## Video Recording (Click Below → Youtube)
+
+[![Watch Video!](https://img.youtube.com/vi/OukWFnTky7o/maxresdefault.jpg)](https://youtu.be/OukWFnTky7o)
+
+## Credits
+
+### Special Thanks to:
+
+Sean Dillon https://github.com/DeviousDoge/
+
+Juliet Lamond https://github.com/jlamond37
+
+### Links Used for Reference:
+
+https://stackoverflow.com/questions/4279611/how-to-embed-a-video-into-github-readme-md
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+
+https://codeburst.io/jsnoob-push-vs-concat-basics-and-performance-comparison-7a4b55242fa9
+
+## Link to Deployed Application
+
+https://fredkk22.github.io/Password-Generator/
--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-// Assign variable to the "#generate" ID in the HTML file
+// Assign a variable to the button's ID
 var generateBtn = document.querySelector('#generate');
 
-// Generate password using the generatePassword() function upon clicking the button
+// Generate random password upon clicking the button
 function generatePassword() {
-  // Declare all variables to be used in the function (excluding Lines 25-28)
+
+  // Declare/Define Variables
   var passwordResult = "";
   var passwordLength;
   var userChoices = [];
   var randUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"];
   var randLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
-  var randSpecChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"];
+  var randSpec = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"];
   var randNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  // First, prompt the user to enter the length of their password
+
+  // Prompt Section
   passwordLength = prompt("Please enter the length of your desired password:");
-  // Determine if the value entered for "passwordLength" was sufficient
+
   if (!passwordLength) {
     return "Your Secure Password";
   } else if (8 > passwordLength || passwordLength > 128) {
@@ -21,32 +23,34 @@ function generatePassword() {
     return "Your Secure Password";
   } else { }
 
-  var confirmPassUpper = confirm("Would you like uppercase letters in your password?");
-  var confirmPassLower = confirm("Would you like lowercase letters in your password?");
-  var confirmPassNum = confirm("Would you like numbers in your password?");
-  var confirmPassSpec = confirm("Would you like special characters in your password?");
+  // Confirm Section
+  var confirmUpper = confirm("Would you like uppercase letters in your password?");
+  var confirmLower = confirm("Would you like lowercase letters in your password?");
+  var confirmNum = confirm("Would you like numbers in your password?");
+  var confirmSpec = confirm("Would you like special characters in your password?");
 
-  if (confirmPassUpper) {
+  if (confirmUpper) {
     userChoices = randUpper.concat(userChoices);
   }
 
-  if (confirmPassLower) {
+  if (confirmLower) {
     userChoices = randLower.concat(userChoices);
   }
 
-  if (confirmPassNum) {
+  if (confirmNum) {
     userChoices = randNum.concat(userChoices);
   }
 
-  if (confirmPassSpec) {
-    userChoices = randSpecChar.concat(userChoices);
+  if (confirmSpec) {
+    userChoices = randSpec.concat(userChoices);
   }
 
-  if (!confirmPassUpper && !confirmPassLower && !confirmPassNum && !confirmPassSpec) {
+  if (!confirmUpper && !confirmLower && !confirmNum && !confirmSpec) {
     alert("Please pick at least one option");
     return "Your Secure Password";
   } else { }
 
+  // For Loop Section
   for (i = 0; i < passwordLength; i++) {
     passwordResult = passwordResult + userChoices[Math.floor(Math.random() * userChoices.length)];
   }
@@ -62,7 +66,6 @@ function writePassword() {
 
   passwordText.value = password;
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
